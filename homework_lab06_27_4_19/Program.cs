99b0a5805a64a0baedde00c71ca865db56d948c5
Emmanuel Badejo <EBadejo@spartaglobal.com>
2019-04-29 10:24:29 +0100

    initial commit

--- homework_lab06_27_4_19/Program.cs
+++ homework_lab06_27_4_19/Program.cs
@@ -10,6 +10,32 @@ namespace homework_lab06_27_4_19
     {
         static void Main(string[] args)
         {
+
+            string[] array01 = new string[5];
+            array01[0] = "Afternoon";
+            array01[1] = "Hi";
+            array01[2] = "Howdy";
+            array01[3] = "Bye";
+            array01[4] = "Adios";
+
+
+            string[] array02 = array01;
+
+
+            foreach (string value in array01)
+            {
+                Console.WriteLine(value);
+
+
+
+            }
+
+            foreach(string value2 in array02)
+            {
+                Console.WriteLine(value2);
+            }
+            
         }
     }
+
 }
